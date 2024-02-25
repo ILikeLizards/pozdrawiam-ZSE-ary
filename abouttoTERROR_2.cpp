3bb8af1ff573602c51bc2ded9b5c4dc8f37d121f
ILikeLizards <161225408+ILikeLizards@users.noreply.github.com>
2024-02-25 15:43:55 +0100

    Add files via upload

---
+++ abouttoTERROR_2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+class Pracownik {
+public:
+    string imie;
+    string nazwisko;
+    void wyswietldane() {
+        cout << imie;
+        cout << nazwisko;
+    }
+};
+int main()
+{
+    Pracownik pracownik;
+    Pracownik* w_pracownik = nullptr;
+    w_pracownik = new pracownik();
+    w_pracownik->imie = "jan";
+    w_pracownik->nazwisko = "kowalski";
+    delete w_pracownik;
+}
---
+++ shinshinjuandzing.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+class funkcje{
+private:
+    static const int rozmiar= 50;
+    int liczby[rozmiar + 1];
+
+public:
+ 
+    void losowanie() {
+ /*******************************************************
+                    nazwa funkcji : wyswietlanie
+                    parametry wejściowe : brak
+                    wartość zwracana : nic
+                    informacje : wypełnia tablice losowyni liczbami
+                    autor : <imię i nazwisko, klasa>
+ ****************************************************/
+
+
+        int i;
+
+        srand(time(NULL));
+        for (i = 0; i < rozmiar; i++) {
+            liczby[i] = rand() % 100 + 1;
+        }
+
+        liczby[rozmiar] = -1;
+    }
+    void wyswietlanie() {
+/*******************************************************
+            nazwa funkcji : wyswietlanie
+            parametry wejściowe : brak
+            wartość zwracana : nic
+            informacje : wyswietla liczby w tablicy
+            autor : <imię i nazwisko, klasa>
+****************************************************/
+        int i;
+
+        cout << "liczby w tablicy";
+
+        for (i = 0; i < rozmiar; i++) {
+            cout << liczby[i];
+            if (i != rozmiar - 1) {
+                cout << ", ";
+            }
+        }
+    }
+    string wyszukiwanie(int szukana) {
+/*******************************************************
+                    nazwa funkcji : wyszikuwanie
+                    parametry wejściowe : szukana
+                    wartość zwracana : tekst
+                    informacje :szuka wpisanej liczby w tablicy
+                    autor : <imię i nazwisko, klasa>
+****************************************************/
+        int i = 0;
+        string tekst;
+
+        liczby[rozmiar] = szukana;
+
+        while (liczby[i] != szukana) {
+            i++;
+        }
+
+        i++;
+
+        if (i >= rozmiar) {
+            tekst= "liczby nie ma";
+        }
+        else {
+            tekst = "liczba znajduję się na " + to_string(i) + " miejscu w tablicy";
+        }
+
+        return(tekst);
+    }
+};
+
+
+int main() {
+    
+    funkcje tablica;
+    int szukana;
+    int x;
+
+    tablica.losowanie();
+
+    cout << "Podaj szukaną liczbę(1=100)";
+    cin >> szukana;
+
+    tablica.wyswietlanie();
+
+    cout << tablica.wyszukiwanie(szukana);
+    return 0;
+}
